00d817ea5111b6b621daa8f9640249c6f6ace50e
Oleh Shchyhol <shchyhol@gmil.com>
2022-06-13 02:08:52 +0100

    Finish Chat

--- Chatroom/public/code.js
+++ Chatroom/public/code.js
@@ -19,9 +19,9 @@
 
     });
 
-    app.querySelector(".chat-screen #send-message").addEventListener("click", function{
+    app.querySelector(".chat-screen #send-message").addEventListener("click", function(){
 
-        let message = app.querySelector(".chat-screen #message-imput").value;
+        let message = app.querySelector(".chat-screen #message-input").value;
         if(message.length == 0){
             return;
         }
@@ -35,13 +35,54 @@
             text:message
         });
 
-        app.querySelector(".chat-screen #message-imput").value = "";
+        app.querySelector(".chat-screen #message-input").value = "";
+    });
+        app.querySelector(".chat-screen #exit-chat").addEventListener("click", function(){
+
+            socket.emit("exituser", uname);
+            window.Location.href = window.Location.href;
+        });
+
+        socket.on("update", function(update){
+            renderMessage("update", update);
+        });
+
+        socket.on("chat", function(message){
+            renderMessage("other", message);
+        });
 
         function renderMessage(type, message){
             let messageConteiner = app.querySelector(".chat-screen .messages");
-            if
+            if(type == "my"){
+                let el = document.createElement("div");
+                el.setAttribute("class", "message my-message");
+                el.innerHTML = `
+                    <div>
+                        <div class="name">You</div>
+                        <div class="text">${message.text}</div>
+                    </div>
+                `;
+                messageConteiner.appendChild(el);
+            } else if(type == "other"){
+                let el = document.createElement("div");
+                el.setAttribute("class", "message other-message");
+                el.innerHTML = `
+                    <div>
+                        <div class="name">${message.username}</div>
+                        <div class="text">${message.text}</div>
+                    </div>
+                `;
+                messageConteiner.appendChild(el);
+            } else if(type =="update"){
+                let el = document.createElement("div");
+                el.setAttribute("class", "update");
+                el.innerText = message;
+                messageConteiner.appendChild(el);
+            }
+            // scroll chat to end
+            messageConteiner.scrollTop = messageConteiner.scrollHeight - messageConteiner.clientHeight;
         }
 
-    });
+
 
 })();
