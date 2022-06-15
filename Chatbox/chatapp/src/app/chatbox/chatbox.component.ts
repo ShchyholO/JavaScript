66cf3c3c20c3ce570329caede954609d918d3b91
Oleh Shchyhol <shchyhol@gmil.com>
2022-06-15 01:53:44 +0100

    d

--- Chatbox/chatapp/src/app/chatbox/chatbox.component.ts
+++ Chatbox/chatapp/src/app/chatbox/chatbox.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import io from 'socket.io-client';
 
+const SOCKET_ENDPOINT = 'localhost:3000';
 @Component({
   selector: 'app-chatbox',
   templateUrl: './chatbox.component.html',
   styleUrls: ['./chatbox.component.css']
 })
 export class ChatboxComponent implements OnInit {
-
+  socket: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.setupSocketConection();
   }
-
+  setupSocketConection(){
+    this.socket = io(SOCKET_ENDPOINT);
+  }
+  sendMsg(){
+    this.socket.emit('message', this.message);
+    this.message = '';
+  }
 }
