f4f7f2a80f2cc9a4cc292c98a75418c3347e6f74
Oleh Shchyhol <shchyhol@gmil.com>
2022-06-22 02:02:59 +0100

    PeopleCountApp

---
+++ PeopleCounterApp/script.js
@@ -0,0 +1,13 @@
+// document.getElementById('count-el').innerText = 5;
+
+let myFunction = function(myDogAge){
+    myDogAge += 1;
+}
+
+let myDogAge = document.addEventListener('click', myFunction);
+ 
+console.log(myFunction(1));
+
+ document.getElementById('count-el').innerText = 2;
+
+ 
