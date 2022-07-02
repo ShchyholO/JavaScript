d0d933434c7cd31b14cadb57cfcf4a75bee9c216
Oleh Shchyhol <shchyhol@gmil.com>
2022-07-02 02:00:35 +0100

    Finish

--- BlackJackGame/script.js
+++ BlackJackGame/script.js
@@ -1,4 +1,7 @@
-
+let player = {
+    name: "Oleh",
+    chips: 145 
+    }
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -7,14 +10,11 @@ let message = "";
 let messageEl = document.getElementById('message-el');
 let sumEl = document.getElementById('sum-el');
 let cardsEl = document.getElementById('cards-el');
-
-let player = {
-    name: "Oleh",
-    chips: 145 
-}
-
-
 let playerEl = document.getElementById('player-el');
+
+
+
+
 playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard(){
@@ -70,7 +70,5 @@ function newCard(){
     renderGame();
     }
 }
-
-
 
 
